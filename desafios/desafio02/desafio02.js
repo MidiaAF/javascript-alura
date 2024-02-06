4342148a0706452d62354b1996cd000eaa81bb8c
Lorena200216 <midia.faustino@scania.com>
2024-02-06 07:17:59 -0300

    novo curso

--- desafios/desafio02/desafio02.js
+++ desafios/desafio02/desafio02.js
@@ -8,7 +8,15 @@ let paragrafoElemento = document.querySelector('p');// Cria um elemento de pará
 paragrafoElemento.innerHTML = saudacao();// Define o texto do parágrafo usando a função saudacao()
 
 
-///**Desafio 2 = Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+//Desafio 2 - Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+function exibirOlaNome(nome) {
+  console.log(`Olá, ${nome}!`);
+}
+
+exibirOlaNome("Alice");
+
+
+///**Desafio 3 = Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
 function dobrarNumero(numero){
     return numero * 2; // dobrar numero 
 }
@@ -17,7 +25,7 @@ let resultado = dobrarNumero(20);// Substitua 5 pelo número que deseja dobrar
 console.log(resultado);// Isso imprimirá o dobro do número no console*/
 
 
-//**Desafio 3 = Criar uma função que recebe três números como parâmetros e retorna a média deles.
+//**Desafio 4 = Criar uma função que recebe três números como parâmetros e retorna a média deles.
 
 function calcularMedia(num1, num2, num3) {
    let somaValores = (num1 + num2 + num3 )/ 3; 
@@ -28,7 +36,7 @@ let resultadoMedia = calcularMedia( 50, 30 , 40  );
 console.log(resultadoMedia);
 
 
-//** Desafio 4 =  Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+//** Desafio 5 =  Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
 let num1 = 10;
 let num2 = 30 ;
 
@@ -44,9 +52,12 @@ let retornarMaiorNumero = calcularMaiorNumero(num1 , num2);
 console.log("O maior número é " + retornarMaiorNumero);
 
 
-//** Desafio 5 = Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
-let numero = 100;
-let multiplicarNumero = numero * numero;
-console.log(multiplicarNumero);
+//** Desafio 6 = Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+
+function quadrado(numero){
+    return numero * numero ;
+}
+let resultado = quadrado(2);
+console.log(resultado);
 
 
--- iniciantes em programação/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ iniciantes em programação/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,26 +1,58 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1; // começar pelo 1 de tentativas
+
 
 function exibirTextoNaTela(tag, texto) { // criando a função dos textos 
     let campo = document.querySelector(tag); // tag o que eles tem em comum e texto o que tem diferente
     campo.innerHTML = texto;//descrever texto 
 }
 
-exibirTextoNaTela('h1' , 'Jogo do número secreto'); // chamando a function criada sobre os textos
-exibirTextoNaTela ('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1' , 'Jogo do número secreto'); // chamando a function criada sobre os textos
+    exibirTextoNaTela ('p', 'Escolha um número entre 1 e 10');
+}
+exibirMensagemInicial();
+
 
 function verificarChute() { // 
     let chute = document.querySelector('input').value; // pegar informação o valor digitado
+    if ( chute  == numeroSecreto) { // chutar numero 
+        exibirTextoNaTela('h1', 'Acertou');
+        let palavraTentativa =  tentativas > 1 ? 'tentativas' : 'tentativa' ;  // se numeroque colocar for maior que 1 tentativas menor ou tentativa 
+        let mensagemTentativas = `Voc ê descobriu o número secreto com ${tentativas} ${palavraTentativa}!`; // numero: 1 de tentativas 
+        exibirTextoNaTela('p', mensagemTentativas );
+        document.getElementById('reiniciar').removeAttribute('disabled');//desabilitar um button
+    }else{
+        if(chute > numeroSecreto){
+           // exibirTextoNaTela('h1', 'Errou');
+            exibirTextoNaTela ('p', 'O número secreto é menor ');
+        }else{
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;//incrementar quantidade de tentativas
+        limparCampo();
+    }
+
     console.log(chute == numeroSecreto);
-   //console.log(numeroSecreto); // numero esta secrteo quando clicar em chutar 
+   //console.log(numeroSecreto); // numero esta secreto quando clicar em chutar 
     
 }
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);//partInt= numeros inteiros /// 
+    return parseInt(Math.random() * 10 + 1);//partInt= numeros inteiros retornar numeros aleatoriso true e false /// 
 }
 
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
 
-
-
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio(); 
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);// só habilitar quando for um novo jogo 
+}
 
 
 
